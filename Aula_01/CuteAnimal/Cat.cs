d51a8367529b97fac0bffadb7499bbf58f85fc7c
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2021-09-28 12:37:17 +0100

    Exercise not done lmao

--- Aula_01/CuteAnimal/Cat.cs
+++ Aula_01/CuteAnimal/Cat.cs
@@ -1,15 +1,39 @@
+using System;
+
 namespace CuteAnimal
 {
     public class Cat
-    {
+    {   
         private readonly string name;
         private int energy;
         private Feed feed;
         private Mood mood;
 
-        public void Eat();
-        public void Sleep();
-        public void Meow();
-        public void Play();
+        // X
+        public void Eat()
+        {
+            
+        }
+        
+        // X
+        public void Sleep()
+        {
+            Console.WriteLine("How long is the Cat resting?");
+            int sleepTime = Convert.ToInt32( Console.ReadLine());
+            energy = energy + sleepTime;
+        }
+        
+        // X
+        public void Meow()
+        {
+
+        }
+        
+        // X
+        public void Play()
+        {
+
+        }
+        
     }
 }
